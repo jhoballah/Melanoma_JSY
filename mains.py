9d2c6ea2091783100d3d83c89c9b0ceca63ae270
VCM admin account <vcm@vcm-1856.vm.duke.edu>
2017-12-05 22:56:55 -0500

    was having fun (ironic commit at 11pm?) and decided the change the return message

--- mains.py
+++ mains.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return 'holla at ya girl, flaskapps are simple'
 
 if __name__=="__main__":
 	app.run(port="5900",host="0.0.0.0")
