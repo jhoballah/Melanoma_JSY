1ca00c88d97a45bd502b36604678bef9aac5754c
ss842 <ss842@duke.edu>
2017-12-09 15:38:59 -0500

    new attempt at getting rank 4

--- image_send_flask.py
+++ image_send_flask.py
@@ -43,15 +43,15 @@ def image_send():
 
     # msg = msg[msg.find(b"<plain_txt_msg:img>") + len(b"<plain_txt_msg:img>"):
     # msg.find(b"<!plain_txt_msg>")]
-    msg = base64.b64decode(image_b64_data[0])
+    msg = base64.b64decode(image_b64_data[0].encode('utf-8')[2:-1])
     buf = io.BytesIO(msg)
     img = np.asarray(Image.open(buf).convert('L'))
-    image = []
-    img1 = img[np.newaxis,:,:]
-    image.append[img1]
+    #image = []
+    #img1 = img[np.newaxis,:,:]
+    #image.append[img1]
     # return img
-    
-    image = np.array(image)
+    image = np.array(buf)
+    #image = np.array(image)
 
     (label, prediction) = get_prediction(image)
 
